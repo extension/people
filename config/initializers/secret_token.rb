8893443805a26a32a148b55f0f0bf8044b8ab993
Jason Adam Young <jayoung@extension.org>
2012-09-12 22:10:33 -0400

    rails new people
    + .gitignore copied from positronic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+People::Application.config.secret_token = '41e1ec87b7f8600e8d793a7c895e6d474a4f1c3f4ce7b5fd1cd5e9a74a89e21ae1cacddc9818017ed0523348463434757c5e7de3721bd6360156156463f2c6f4'
