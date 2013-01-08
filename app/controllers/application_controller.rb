7fe6eb9c661689110a35d7ac8c024ee60d097df5
Jason Adam Young <jayoung@extension.org>
2013-01-08 16:48:40 -0500

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,46 +9,5 @@ class ApplicationController < ActionController::Base
   include AuthLib
   protect_from_forgery
   has_mobile_fu false
-  helper_method :current_person
-  before_filter :signin_required
-
-
-  def set_current_person(person)
-    if(person.blank?)
-      @current_person = nil
-      reset_session
-    else
-      @current_person = person
-      session[:person_id] = person.id
-    end
-  end
-
-  def current_person
-    if(!@current_person)
-      if(session[:person_id])
-        @current_person = Person.find_by_id(session[:person_id])
-      end
-    end
-    @current_person
-  end
-
-  # move to the last url stored by store_location call or to the passed default one
-  def redirect_back_or_default(default)
-    if session[:return_to].nil?
-      redirect_to default
-    else
-      redirect_to session[:return_to]
-      session[:return_to] = nil
-    end
-  end
-
-
-  def admin_mode?
-    if(current_person.present? && current_person.is_admin?)
-      return true
-    else
-      return false
-    end
-  end
-  
+  before_filter :signin_required 
 end
