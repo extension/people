16b7b3fed5a0e93318a78ee551b5c40394e14f20
Jason Adam Young <jayoung@extension.org>
2013-03-13 13:48:32 -0400

    refactor mailers

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -4,82 +4,32 @@
 # === LICENSE:
 # see LICENSE file
 
-class AccountMailer < ActionMailer::Base
-  helper MailerHelper
-  default_url_options[:host] = Settings.urlwriter_host
-  default from: Settings.email_from_address
-  default bcc: Settings.email_bcc_address
-  helper_method :ssl_root_url, :ssl_webmail_logo, :is_demo?
-  
+class AccountMailer < BaseMailer
 
   def signup(options = {})
     @person = options[:person]
     @subject = "Please confirm your email address"
-    @will_cache_email = options[:cache_email].nil? ? true : options[:cache_email]
+    @save_sent_email = options[:save_sent_email].nil? ? true : options[:save_sent_email]
     
     if(!@person.email.blank?)
-      if(@will_cache_email)
-        # create a cached mail object that can be used for "view this in a browser" within
-        # the rendered email.
-        @mailer_cache = MailerCache.create(person: @person, cacheable: @person)
-      end
-      
       return_email = mail(to: @person.email, subject: @subject)
-      
-      if(@mailer_cache)
-        # now that we have the rendered email - update the cached mail object
-        @mailer_cache.update_attribute(:markup, return_email.body.to_s)
-      end
+      save_sent_email_for_person(return_email,@person) if @save_sent_email
     end
     
-    # the email if we got it
-    return_email
+    return_email   
   end
 
   def welcome(options={})
     @person = options[:person]
     @subject = "Welcome!"
-    @will_cache_email = options[:cache_email].nil? ? true : options[:cache_email]
+    @save_sent_email = options[:save_sent_email].nil? ? true : options[:save_sent_email]
     
     if(!@person.email.blank?)
-      if(@will_cache_email)
-        # create a cached mail object that can be used for "view this in a browser" within
-        # the rendered email.
-        @mailer_cache = MailerCache.create(person: @person, cacheable: @person)
-      end
-      
       return_email = mail(to: @person.email, subject: @subject)
-      
-      if(@mailer_cache)
-        # now that we have the rendered email - update the cached mail object
-        @mailer_cache.update_attribute(:markup, return_email.body.to_s)
-      end
+      save_sent_email_for_person(return_email,@person) if @save_sent_email
     end
     
-    # the email if we got it
     return_email    
-  end
-
-
-  def ssl_root_url
-    if(Settings.app_location != 'localdev')
-      root_url(protocol: 'https')
-    else
-      root_url
-    end
-  end
-
-  def ssl_webmail_logo
-    parameters = {mailer_cache_id: @mailer_cache.id, format: 'png'}
-    if(Settings.app_location != 'localdev')
-      webmail_logo_url(parameters.merge({protocol: 'https'}))
-    else
-      webmail_logo_url(parameters)
-    end
-  end
-
-  def is_demo?
-    Settings.app_location != 'production'
   end
 
 end
---
+++ app/mailers/base_mailer.rb
@@ -0,0 +1,39 @@
+# === COPYRIGHT:
+# Copyright (c) North Carolina State University
+# Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+# see LICENSE file
+
+class BaseMailer < ActionMailer::Base
+  helper MailerHelper
+  default_url_options[:host] = Settings.urlwriter_host
+  default from: Settings.email_from_address
+  default bcc: Settings.email_bcc_address
+  helper_method :ssl_root_url, :ssl_webmail_logo, :is_demo?
+  
+
+  def save_sent_email_for_person(email,person)
+    SentEmail.create(person: person, markup: email.body.to_s)
+  end
+
+  def ssl_root_url
+    if(Settings.app_location != 'localdev')
+      root_url(protocol: 'https')
+    else
+      root_url
+    end
+  end
+
+  def ssl_webmail_logo
+    parameters = {mailer_cache_id: @mailer_cache.id, format: 'png'}
+    if(Settings.app_location != 'localdev')
+      webmail_logo_url(parameters.merge({protocol: 'https'}))
+    else
+      webmail_logo_url(parameters)
+    end
+  end
+
+  def is_demo?
+    Settings.app_location != 'production'
+  end
+end
