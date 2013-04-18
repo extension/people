7f9fadcdab8090396f3797edb213bc3d90ff1b55
Jason Adam Young <jayoung@extension.org>
2013-04-18 16:25:03 -0400

    time zone support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   has_mobile_fu false
   before_filter :signin_required
+  before_filter :set_time_zone_from_user
   before_filter :update_last_activity
   before_filter :check_hold_status 
   helper_method :current_person
@@ -17,6 +18,15 @@ class ApplicationController < ActionController::Base
 
   def update_last_activity
     current_person.touch(:last_activity_at) if current_person
+  end
+
+  def set_time_zone_from_user
+    if(current_person)
+      Time.zone = current_person.time_zone
+    else
+      Time.zone = Settings.default_display_timezone
+    end
+    true
   end
 
 end
