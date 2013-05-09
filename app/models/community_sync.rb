7dedfb92a57ab479417bdd4cd09dcd1ed02c4ff8
Jason Adam Young <jayoung@extension.org>
2013-05-09 12:20:39 -0400

    Create community sync

--- app/models/community_sync.rb
+++ app/models/community_sync.rb
@@ -6,21 +6,24 @@
 #  see LICENSE file
 
 class CommunitySync < ActiveRecord::Base
-  belongs_to :community
   attr_accessible :community, :community_id, :processed, :sync_on_create
 
-
-  UPDATE_DATABASES = {'www_database' => Settings.www_database}
+  UPDATE_DATABASES = {'create_database' => Settings.create_database,
+                      'www_database' => Settings.www_database}
 
   after_create  :queue_update
 
+  belongs_to :community
+
+
   def queue_update
-    if(self.sync_on_create?)
+    if(self.sync_on_create? or !Settings.redis_enabled)
       self.update_communities
     else
       self.delay.update_communities
     end
   end
+
 
   def update_communities
     if(!self.processed?)
@@ -31,6 +34,15 @@ class CommunitySync < ActiveRecord::Base
     end
   end
 
+  def create_database
+    if(self.community.connect_to_drupal)
+      if(self.community.drupal_node_id.blank?)
+        create_insert_community
+      else
+        create_update_community
+      end
+    end
+  end
 
   def www_database
     if(publishing_community = DarmokPublishingCommunity.find_by_id(self.id))
@@ -81,6 +93,243 @@ class CommunitySync < ActiveRecord::Base
     query    
   end
 
+  def create_insert_community
+    community = self.community
+    update_database = UPDATE_DATABASES['create_database']  
+    
+    ## node_revision insertion
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    INSERT INTO #{update_database}.node_revision (nid,uid,title,log,timestamp) 
+    SELECT 0,
+           1,
+           #{ActiveRecord::Base.quote_value(community.name)},
+           'Added by synchronization script',
+           UNIX_TIMESTAMP() 
+    END_SQL
+    self.connection.execute(sql)
+    
+    ## node insertion
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    INSERT INTO #{update_database}.node (vid,type,language,title,uid,created,changed,promote) 
+    SELECT vid,
+           'group',
+           'und',
+           #{update_database}.node_revision.title,
+           1,
+           UNIX_TIMESTAMP(),
+           UNIX_TIMESTAMP(),
+           1 
+    FROM #{update_database}.node_revision 
+    WHERE #{update_database}.node_revision.nid = 0  
+    AND   #{update_database}.node_revision.log = 'Added by synchronization script'
+    AND   #{update_database}.node_revision.title = #{ActiveRecord::Base.quote_value(community.name)}
+    END_SQL
+    self.connection.execute(sql)
+    
+
+    ## set the node_revision node id based on name match
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    UPDATE #{update_database}.node_revision,#{update_database}.node 
+    SET #{update_database}.node_revision.nid = #{update_database}.node.nid 
+    WHERE #{update_database}.node.vid = #{update_database}.node_revision.vid 
+    AND #{update_database}.node.type = 'group' 
+    AND #{update_database}.node_revision.title = #{update_database}.node.title
+    AND #{update_database}.node_revision.title = #{ActiveRecord::Base.quote_value(community.name)}
+    END_SQL
+    self.connection.execute(sql)
+    
+
+    ## set the darmok community association based on name
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    UPDATE #{Community.table_name},#{update_database}.node 
+    SET #{Community.table_name}.drupal_node_id = #{update_database}.node.nid 
+    WHERE #{update_database}.node.type = 'group' 
+    AND   #{Community.table_name}.drupal_node_id IS NULL
+    AND   #{update_database}.node.title = #{ActiveRecord::Base.quote_value(community.name)}
+    AND   #{Community.table_name}.id = #{community.id}
+    END_SQL
+    self.connection.execute(sql)
+
+    # reload now that we have a drupal_node_id
+    community.reload
+
+    if(!community.drupal_node_id.blank?)
+      ## og
+      sql = <<-END_SQL.gsub(/\s+/, " ").strip
+      INSERT INTO #{update_database}.og (gid,etid,entity_type,label,state,created) 
+      SELECT #{community.drupal_node_id},
+             #{community.drupal_node_id},
+             'node',
+             #{ActiveRecord::Base.quote_value(community.name)},
+             1,
+             #{community.created_at.to_i} 
+      END_SQL
+      self.connection.execute(sql)
+
+      ## og user roles
+      # hardcoded roles - do not change these roles in drupal!!  leader = 3 member = 2
+      sql = <<-END_SQL.gsub(/\s+/, " ").strip
+      INSERT IGNORE INTO #{update_database}.og_users_roles (uid,rid,gid) 
+      SELECT #{CommunityConnection.table_name}.person_id,
+             IF(#{CommunityConnection.table_name}.connectiontype = 'leader',3,2),
+             #{update_database}.og.gid 
+      FROM  #{update_database}.og, #{CommunityConnection.table_name} 
+      WHERE #{update_database}.og.etid = #{community.drupal_node_id} 
+        AND #{CommunityConnection.table_name}.community_id = #{community.id}
+        AND #{CommunityConnection.table_name}.connectiontype IN ('leader','member') 
+      END_SQL
+      self.connection.execute(sql)
+
+      # og_membership
+      sql = <<-END_SQL.gsub(/\s+/, " ").strip
+      INSERT IGNORE INTO #{update_database}.og_membership (type, etid, entity_type, gid, state, created) 
+      SELECT 'og_membership_type_default', 
+             #{CommunityConnection.table_name}.person_id, 
+             'user',
+             #{update_database}.og.gid, 
+             '1',
+             UNIX_TIMESTAMP(#{CommunityConnection.table_name}.created_at)
+      FROM #{update_database}.og,#{CommunityConnection.table_name}  
+      WHERE #{update_database}.og.etid = #{community.drupal_node_id} 
+        AND #{CommunityConnection.table_name}.community_id = #{community.id}
+        AND #{CommunityConnection.table_name}.connectiontype IN ('leader','member') 
+      END_SQL
+      self.connection.execute(sql)
+
+
+      # group audience field
+      # etid is field_config_entity_type for 'user' == 3 - hardcoded!  
+      # Note: delta is a hack! there's an primary index on etid+revision_id+deleted+delta+langague and since revision_id == the uid, this means that
+      # every user row gets an incremented delta.  Trying to query on this and insert is a hard problem(tm) (it can be done with max(delta), but you
+      # have to make a couple of passes, and I haven't figured it out yet.  So, I'm setting delta to the gid.  I don't think delta is used in the queries)
+      ['data','revision'].each do |field_table|
+        sql = <<-END_SQL.gsub(/\s+/, " ").strip
+        INSERT IGNORE INTO #{update_database}.field_#{field_table}_group_audience (bundle,deleted,entity_id,revision_id,language,delta,group_audience_gid,group_audience_state,group_audience_created,entity_type) 
+        SELECT 'user',
+               0, 
+               #{CommunityConnection.table_name}.person_id,
+               #{CommunityConnection.table_name}.person_id, 
+               'und',
+               #{update_database}.og.gid,
+               #{update_database}.og.gid,
+               1,
+               UNIX_TIMESTAMP(#{CommunityConnection.table_name}.created_at),
+               'user' 
+          FROM #{update_database}.og,#{CommunityConnection.table_name}  
+          WHERE #{update_database}.og.etid = #{community.drupal_node_id} 
+            AND #{CommunityConnection.table_name}.community_id = #{community.id}
+            AND #{CommunityConnection.table_name}.connectiontype IN ('leader','member') 
+        END_SQL
+        self.connection.execute(sql)
+      end
+
+      # $@#%!^!#%@$!@#$ FIELDS INSERTS
+      ['data','revision'].each do |field_table|
+        sql = <<-END_SQL.gsub(/\s+/, " ").strip
+        INSERT IGNORE INTO #{update_database}.field_#{field_table}_group_group (bundle,deleted,entity_id,revision_id,language,delta,group_group_value,entity_type) 
+          SELECT 'group',
+                 0, 
+                 #{community.drupal_node_id},
+                 #{update_database}.node.vid,
+                 'und',
+                 0,
+                 1,
+                 'node' 
+          FROM #{update_database}.node 
+          WHERE #{update_database}.node.nid = #{community.drupal_node_id} 
+        END_SQL
+        self.connection.execute(sql)
+      end
+
+      ['data','revision'].each do |field_table|
+        sql = <<-END_SQL.gsub(/\s+/, " ").strip
+        INSERT INTO #{update_database}.field_#{field_table}_field_group_designation (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_group_designation_value)
+          SELECT 'node',
+                 'group',
+                 0, 
+                 #{community.drupal_node_id},
+                 #{community.drupal_node_id},
+                 'und', 
+                 0, 
+                 #{community.entrytype}
+          ON DUPLICATE KEY 
+          UPDATE field_group_designation_value=#{community.entrytype}
+        END_SQL
+        self.connection.execute(sql)
+      end      
+    end # if the drupal_node_id exists
+  end
+
+  def create_update_community
+    community = self.community
+    update_database = UPDATE_DATABASES['create_database']  
+    
+    ## update the nodes table
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    UPDATE #{update_database}.node
+    SET #{update_database}.node.title = #{ActiveRecord::Base.quote_value(community.name)},
+        #{update_database}.node.uid = 1 
+    WHERE #{update_database}.node.nid =  #{community.drupal_node_id} 
+    AND #{update_database}.node.type = 'group' 
+    END_SQL
+    self.connection.execute(sql)
+    
+    ## update revisions table
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    UPDATE #{update_database}.node_revision,#{update_database}.node 
+    SET #{update_database}.node_revision.title = #{update_database}.node.title,
+        #{update_database}.node_revision.log = 'Updated by synchronization script',
+        #{update_database}.node_revision.uid = 1 
+    WHERE #{update_database}.node.vid = #{update_database}.node_revision.vid 
+    AND #{update_database}.node.type = 'group'
+    AND #{update_database}.node_revision.title != #{update_database}.node.title
+    AND #{update_database}.node.nid =  #{community.drupal_node_id} 
+    END_SQL
+    self.connection.execute(sql)
+
+    ## og
+    sql = <<-END_SQL.gsub(/\s+/, " ").strip
+    UPDATE #{update_database}.og  
+    SET #{update_database}.og.label = #{ActiveRecord::Base.quote_value(community.name)}
+    WHERE #{update_database}.og.gid = #{community.drupal_node_id}
+    END_SQL
+    self.connection.execute(sql)
+
+    # $@#%!^!#%@$!@#$ FIELDS INSERTS
+    ['data','revision'].each do |field_table|
+      sql = <<-END_SQL.gsub(/\s+/, " ").strip
+      INSERT IGNORE INTO #{update_database}.field_#{field_table}_group_group (bundle,deleted,entity_id,revision_id,language,delta,group_group_value,entity_type) 
+        SELECT 'group',
+               0, 
+               #{community.drupal_node_id},
+               #{update_database}.node.vid,
+               'und',
+               0,
+               1,
+               'node' 
+        FROM #{update_database}.node 
+        WHERE #{update_database}.node.nid = #{community.drupal_node_id} 
+      END_SQL
+      self.connection.execute(sql)
+    end
+
+    ['data','revision'].each do |field_table|
+      sql = <<-END_SQL.gsub(/\s+/, " ").strip
+      INSERT INTO #{update_database}.field_#{field_table}_field_group_designation (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_group_designation_value)
+        SELECT 'node',
+               'group',
+               0, 
+               #{community.drupal_node_id},
+               #{community.drupal_node_id},
+               'und', 
+               0, 
+               #{community.entrytype}
+        ON DUPLICATE KEY 
+        UPDATE field_group_designation_value=#{community.entrytype}
+      END_SQL
+      self.connection.execute(sql)
+    end 
+  end
 
   def value_or_null(value)
     value.blank? ? 'NULL' : value
