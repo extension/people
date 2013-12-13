2eecd0cab4464759d9970a1debac3ea16f31e005
Jason Adam Young <jayoung@extension.org>
2013-12-13 15:02:35 -0500

    Use master for dev deploys
    Does not need to be deployed to prod, only matters locally

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,6 +2,6 @@ set :deploy_to, "/services/people/"
 if(branch = ENV['BRANCH'])
   set :branch, branch
 else
-  set :branch, 'development'
+  set :branch, 'master'
 end
 server 'dev.people.extension.org', :app, :web, :db, :primary => true
