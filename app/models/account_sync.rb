8819e110b6f86a41cc4babb4632821424b3e5901
Jason Adam Young <jayoung@extension.org>
2014-09-09 13:21:55 -0400

    fix sql errors highlighted by mysql strict settings

--- app/models/account_sync.rb
+++ app/models/account_sync.rb
@@ -8,12 +8,12 @@
 class AccountSync < ActiveRecord::Base
   serialize :errors
   attr_accessible :success, :errors
-  attr_accessible :person, :person_id, :processed, :process_on_create 
+  attr_accessible :person, :person_id, :processed, :process_on_create
 
   CREATE_ADMIN_ROLE = 3
   UPDATE_DATABASES = {'aae_database' => Settings.aae_database,
                       'learn_database' => Settings.learn_database,
-                      'create_databases' => [Settings.create_database, Settings.create_bootcamp_database],
+                      'create_databases' => [Settings.create_database],
                       'www_database' => Settings.www_database,
                       'wordpress_databases' => [Settings.about_database, Settings.milfam_database]}
 
@@ -48,7 +48,7 @@ class AccountSync < ActiveRecord::Base
 
   def update_accounts
     if(!self.processed?)
-      begin 
+      begin
         UPDATE_DATABASES.keys.each do |sync_target|
           self.send(sync_target)
         end
@@ -63,7 +63,7 @@ class AccountSync < ActiveRecord::Base
     if(aae_user = AaeUser.find_by_darmok_id(self.person_id))
       self.connection.execute(aae_update_query)
     elsif(aae_user = AaeUser.find_by_email(self.person.email))
-      self.connection.execute(aae_conversion_query)        
+      self.connection.execute(aae_conversion_query)
     else
       self.connection.execute(aae_insert_query)
     end
@@ -74,7 +74,7 @@ class AccountSync < ActiveRecord::Base
     if(learn_learner = LearnLearner.find_by_darmok_id(self.person_id))
       self.connection.execute(learn_update_query)
     elsif(learn_learner = LearnLearner.find_by_email(self.person.email))
-      self.connection.execute(learn_conversion_query)        
+      self.connection.execute(learn_conversion_query)
     else
       self.connection.execute(learn_insert_query)
     end
@@ -107,7 +107,7 @@ class AccountSync < ActiveRecord::Base
       self.connection.execute(wordpress_openid_replace_query(update_database))
       self.connection.execute(wordpress_usermeta_insert_update_query(update_database))
     end
-  end    
+  end
 
 
   def aae_update_query
@@ -115,7 +115,7 @@ class AccountSync < ActiveRecord::Base
     update_database = UPDATE_DATABASES['aae_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     UPDATE #{update_database}.users
-    SET #{update_database}.users.login                = #{quoted_value_or_null(person.idstring)}, 
+    SET #{update_database}.users.login                = #{quoted_value_or_null(person.idstring)},
         #{update_database}.users.first_name           = #{quoted_value_or_null(person.first_name)},
         #{update_database}.users.last_name            = #{quoted_value_or_null(person.last_name)},
         #{update_database}.users.retired              = #{person.retired},
@@ -139,7 +139,7 @@ class AccountSync < ActiveRecord::Base
     UPDATE #{update_database}.users
     SET #{update_database}.users.kind                 = 'User',
         #{update_database}.users.darmok_id            = #{person.id},
-        #{update_database}.users.login                = #{quoted_value_or_null(person.idstring)}, 
+        #{update_database}.users.login                = #{quoted_value_or_null(person.idstring)},
         #{update_database}.users.first_name           = #{quoted_value_or_null(person.first_name)},
         #{update_database}.users.last_name            = #{quoted_value_or_null(person.last_name)},
         #{update_database}.users.retired              = #{person.retired},
@@ -160,7 +160,7 @@ class AccountSync < ActiveRecord::Base
     update_database = UPDATE_DATABASES['aae_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.users (login, first_name, last_name, kind, email, time_zone, darmok_id, is_admin, location_id, county_id, title, needs_search_update, created_at, updated_at)
-    SELECT  #{quoted_value_or_null(person.idstring)}, 
+    SELECT  #{quoted_value_or_null(person.idstring)},
             #{quoted_value_or_null(person.first_name)},
             #{quoted_value_or_null(person.last_name)},
             'User',
@@ -178,20 +178,20 @@ class AccountSync < ActiveRecord::Base
     query
   end
 
-  def aae_authmap_insert_query      
+  def aae_authmap_insert_query
     person = self.person
     update_database = UPDATE_DATABASES['aae_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
-    INSERT IGNORE INTO #{update_database}.authmaps (user_id, authname, source, created_at, updated_at) 
-    SELECT #{update_database}.users.id, 
-           CONCAT('https://people.extension.org/',#{update_database}.users.login), 
-           'people', 
-           #{update_database}.users.created_at, 
+    INSERT IGNORE INTO #{update_database}.authmaps (user_id, authname, source, created_at, updated_at)
+    SELECT #{update_database}.users.id,
+           CONCAT('https://people.extension.org/',#{update_database}.users.login),
+           'people',
+           #{update_database}.users.created_at,
            NOW()
     FROM #{update_database}.users
     WHERE #{update_database}.users.darmok_id = #{person.id}
     END_SQL
-    query    
+    query
   end
 
   def learn_update_query
@@ -229,33 +229,33 @@ class AccountSync < ActiveRecord::Base
     person = self.person
     update_database = UPDATE_DATABASES['learn_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
-    INSERT INTO #{update_database}.learners (name, email, has_profile, time_zone, darmok_id, is_admin, created_at, updated_at) 
+    INSERT INTO #{update_database}.learners (name, email, has_profile, time_zone, darmok_id, is_admin, created_at, updated_at)
     SELECT  #{quoted_value_or_null(person.fullname)},
             #{quoted_value_or_null(person.email)},
             1,
             #{quoted_value_or_null(person.time_zone(false))},
             #{person.id},
-            #{value_or_null(person.is_admin_for_application('learn'))},
+            #{person.is_admin_for_application('learn')},
             #{quoted_value_or_null(person.created_at.to_s(:db))},
             NOW()
     END_SQL
     query
   end
 
-  def learn_authmap_insert_query      
+  def learn_authmap_insert_query
     person = self.person
     update_database = UPDATE_DATABASES['learn_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
-    INSERT IGNORE INTO #{update_database}.authmaps (learner_id, authname, source, created_at, updated_at) 
-    SELECT #{update_database}.learners.id, 
-           CONCAT('https://people.extension.org/',#{ActiveRecord::Base.quote_value(person.idstring)}), 
-           'people', 
-           #{update_database}.learners.created_at, 
+    INSERT IGNORE INTO #{update_database}.authmaps (learner_id, authname, source, created_at, updated_at)
+    SELECT #{update_database}.learners.id,
+           CONCAT('https://people.extension.org/',#{ActiveRecord::Base.quote_value(person.idstring)}),
+           'people',
+           #{update_database}.learners.created_at,
            NOW()
     FROM #{update_database}.learners
     WHERE #{update_database}.learners.darmok_id = #{person.id}
     END_SQL
-    query    
+    query
   end
 
 
@@ -263,17 +263,18 @@ class AccountSync < ActiveRecord::Base
     person = self.person
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.users (uid,name,pass,mail,created,status)
-    SELECT  #{person.id}, 
+    SELECT  #{person.id},
             #{quoted_value_or_null(person.idstring)},
             #{ActiveRecord::Base.quote_value(Settings.create_password_string)},
             #{quoted_value_or_null(person.email)},
-            #{quoted_value_or_null(person.created_at.to_s(:db))},
+            UNIX_TIMESTAMP('#{person.created_at.to_s(:db)}'),
             #{(person.validaccount? ? 1 : 0)}
-    ON DUPLICATE KEY 
+    ON DUPLICATE KEY
     UPDATE name=#{quoted_value_or_null(person.idstring)},
            pass=#{ActiveRecord::Base.quote_value(Settings.create_password_string)},
            mail=#{quoted_value_or_null(person.email)},
-           status=#{(person.validaccount? ? 1 : 0)}
+           status=#{(person.validaccount? ? 1 : 0)},
+           created=UNIX_TIMESTAMP('#{person.created_at.to_s(:db)}')
     END_SQL
     query
   end
@@ -292,7 +293,7 @@ class AccountSync < ActiveRecord::Base
     person = self.person
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.users_roles (uid,rid)
-    SELECT  #{person.id}, 
+    SELECT  #{person.id},
             #{CREATE_ADMIN_ROLE}
     END_SQL
     query
@@ -302,34 +303,34 @@ class AccountSync < ActiveRecord::Base
     person = self.person
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.field_#{data_or_revision}_field_#{name}_name (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_#{name}_name_value, field_#{name}_name_format)
-    SELECT 'user', 
-           'user', 
-           0, 
-           #{person.id}, 
-           #{person.id}, 
-           'und', 
-           0, 
-           #{quoted_value_or_null(person.send("#{name}_name"))}, 
+    SELECT 'user',
+           'user',
+           0,
+           #{person.id},
+           #{person.id},
+           'und',
+           0,
+           #{quoted_value_or_null(person.send("#{name}_name"))},
            'NULL'
-    ON DUPLICATE KEY 
+    ON DUPLICATE KEY
     UPDATE field_#{name}_name_value=#{quoted_value_or_null(person.send("#{name}_name"))}
     END_SQL
-    query  
+    query
   end
 
-  def create_authmap_insert_query(update_database)      
+  def create_authmap_insert_query(update_database)
     person = self.person
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.authmap (aid,uid,authname,module)
     SELECT #{person.id},
-           #{person.id},  
-           CONCAT('https://people.extension.org/',#{ActiveRecord::Base.quote_value(person.idstring)}), 
+           #{person.id},
+           CONCAT('https://people.extension.org/',#{ActiveRecord::Base.quote_value(person.idstring)}),
            'openid'
     ON DUPLICATE KEY
     UPDATE uid=#{person.id},
            authname=CONCAT('https://people.extension.org/',#{ActiveRecord::Base.quote_value(person.idstring)})
     END_SQL
-    query    
+    query
   end
 
 
@@ -338,7 +339,7 @@ class AccountSync < ActiveRecord::Base
     update_database = UPDATE_DATABASES['www_database']
     query = <<-END_SQL.gsub(/\s+/, " ").strip
     INSERT INTO #{update_database}.people (id,uid,first_name,last_name,is_admin,retired,created_at,updated_at)
-    SELECT  #{person.id}, 
+    SELECT  #{person.id},
             #{ActiveRecord::Base.quote_value(person.openid_url)},
             #{quoted_value_or_null(person.first_name)},
             #{quoted_value_or_null(person.last_name)},
@@ -346,7 +347,7 @@ class AccountSync < ActiveRecord::Base
             #{person.is_admin_for_application('www')},
             #{ActiveRecord::Base.quote_value(person.created_at.to_s(:db))},
             #{ActiveRecord::Base.quote_value(person.updated_at.to_s(:db))}
-    ON DUPLICATE KEY 
+    ON DUPLICATE KEY
     UPDATE uid=#{ActiveRecord::Base.quote_value(person.openid_url)},
            first_name=#{quoted_value_or_null(person.first_name)},
            last_name=#{quoted_value_or_null(person.last_name)},
@@ -368,7 +369,7 @@ class AccountSync < ActiveRecord::Base
            #{ActiveRecord::Base.quote_value(person.idstring)},
            #{ActiveRecord::Base.quote_value(person.fullname)}
     END_SQL
-    query  
+    query
   end
 
   def wordpress_openid_replace_query(update_database)
@@ -379,7 +380,7 @@ class AccountSync < ActiveRecord::Base
            #{person.id},
            #{ActiveRecord::Base.quote_value(person.openid_url)}
     END_SQL
-    query  
+    query
   end
 
   def wordpress_usermeta_insert_update_query(update_database)
@@ -391,7 +392,7 @@ class AccountSync < ActiveRecord::Base
       capability_string = PHP.serialize({"administrator"=>true})
     else
       capability_string = PHP.serialize({"editor"=>true})
-    end      
+    end
 
     # does a row exist? then update, else insert
     result = self.connection.execute("SELECT * from #{update_database}.wp_usermeta WHERE user_id = #{person.id} and meta_key = 'wp_capabilities'")
@@ -405,7 +406,7 @@ class AccountSync < ActiveRecord::Base
 
     else
       query = <<-END_SQL.gsub(/\s+/, " ").strip
-      UPDATE #{update_database}.wp_usermeta 
+      UPDATE #{update_database}.wp_usermeta
       SET meta_value = #{ActiveRecord::Base.quote_value(capability_string)}
       WHERE user_id = #{person.id} AND meta_key = 'wp_capabilities'
       END_SQL
@@ -416,6 +417,7 @@ class AccountSync < ActiveRecord::Base
   def value_or_null(value)
     value.blank? ? 'NULL' : value
   end
+
 
   def quoted_value_or_null(value)
     value.blank? ? 'NULL' : ActiveRecord::Base.quote_value(value)
