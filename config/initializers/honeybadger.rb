ae9ab2b6ebd0163803c35cf8aa7df414cd306075
Jason Adam Young <jayoung@extension.org>
2015-06-10 10:45:07 -0400

    comment out honeybadger for now

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,5 +1,6 @@
-Honeybadger.configure do |config|
-  config.api_key = Settings.honeybadger_api_key
-  config.environment_name = Settings.app_location
-  config.ignore       << "ActionController::UnknownHttpMethod"
-end
+# Comment out for now
+# Honeybadger.configure do |config|
+#   config.api_key = Settings.honeybadger_api_key
+#   config.environment_name = Settings.app_location
+#   config.ignore       << "ActionController::UnknownHttpMethod"
+# end
