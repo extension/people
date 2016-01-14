d16ffdfa4cca5904ac5a80e00f179535965635c5
Jason Adam Young <jayoung@extension.org>
2016-01-13 19:00:58 -0500

    fixes
    css magic needed

--- app/models/community.rb
+++ app/models/community.rb
@@ -11,6 +11,7 @@ class Community < ActiveRecord::Base
   attr_accessible :creator, :created_by
   attr_accessible :name, :description, :location, :location_id, :memberfilter, :connect_to_drupal
   attr_accessible :connect_to_google_apps, :entrytype, :shortname, :publishing_community, :is_public
+  attr_accessible :community_masthead, :community_masthead_cache, :remove_community_masthead
 
   mount_uploader :community_masthead, CommunityMastheadUploader
 
