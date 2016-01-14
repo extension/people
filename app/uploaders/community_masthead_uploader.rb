6e8a71301f9f492dc208be688966af69d186f02a
Ben MacNeill <ben.macneill@gmail.com>
2016-01-14 16:06:27 -0500

    Fix upload size. The default image was the wrong size.

--- app/uploaders/community_masthead_uploader.rb
+++ app/uploaders/community_masthead_uploader.rb
@@ -33,7 +33,7 @@ class CommunityMastheadUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :large do
-    process :resize_to_fill => [930, 340]
+    process :resize_to_fill => [920, 340]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
