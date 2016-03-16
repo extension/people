531156cd409bb35d68c9bb1ede9da1931b968374
Jason Adam Young <jayoung@extension.org>
2016-03-16 14:37:16 -0400

    learn models

--- app/models/learn/learn_learner.rb
+++ app/models/learn/learn_learner.rb
@@ -11,4 +11,9 @@ class LearnLearner < ActiveRecord::Base
   self.table_name='learners'
 
   has_many :event_activities, class_name: 'LearnEventActivity',foreign_key: 'learner_id'
+  has_many :presenter_connections, :class_name => "LearnPresenterConnection",foreign_key: 'learner_id'
+  has_many :presented_events, through: :presenter_connections, :source => :event, :order => 'position'
+
+  has_many :created_events, :class_name => "LearnEvent",foreign_key: 'creator_id'
+
 end
