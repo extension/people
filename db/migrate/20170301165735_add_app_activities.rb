c302cdba12d74019f90d084ab1fae7e8b265beb0
Jason Adam Young <jayoung@extension.org>
2017-03-03 15:03:18 -0500

    table changes for app activity

--- db/migrate/20170301165735_add_app_activities.rb
+++ db/migrate/20170301165735_add_app_activities.rb
@@ -7,11 +7,14 @@ class AddAppActivities < ActiveRecord::Migration
       t.integer  "section_id",       :default => 1
       t.string   "section_label",    :limit => 25
       t.integer  "activity_code",    :default => 0
-      t.string   "activity_label",   :limit => 25
+      t.string   "activity_label",       :limit => 25
+      t.string   "app_activity_label",   :limit => 25
       t.integer  "item_id",          :default => 0
       t.integer  "item_revision_id", :default => 0
+      t.string   "item_fingerprint",      :limit => 64
       t.integer  "source_id", :default => 0
       t.string   "source_model"
+      t.string   "source_table"
       t.string   "ip_address",       :limit => 45
       t.string   "fingerprint",      :limit => 64
       t.text     "additionaldata"
@@ -21,5 +24,7 @@ class AddAppActivities < ActiveRecord::Migration
 
     add_index "app_activities", ["activity_at", "person_id", "app_id", "section_id", "activity_code","ip_address"], :name => "fields_ndx"
     add_index "app_activities", ["fingerprint"], :name => "fingerprint_ndx", :unique => true
+    add_index "app_activities", ["item_fingerprint"], :name => "item_fingerprint_ndx"
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -53,19 +53,22 @@ ActiveRecord::Schema.define(:version => 20170301165735) do
   add_index "admin_roles", ["person_id", "applabel"], :name => "admin_ndx", :unique => true
 
   create_table "app_activities", :force => true do |t|
-    t.integer  "person_id",                      :default => 0
-    t.integer  "app_id",                         :default => 0
-    t.string   "app_label",        :limit => 25
-    t.integer  "section_id",                     :default => 1
-    t.string   "section_label",    :limit => 25
-    t.integer  "activity_code",                  :default => 0
-    t.string   "activity_label",   :limit => 25
-    t.integer  "item_id",                        :default => 0
-    t.integer  "item_revision_id",               :default => 0
-    t.integer  "source_id",                      :default => 0
+    t.integer  "person_id",                        :default => 0
+    t.integer  "app_id",                           :default => 0
+    t.string   "app_label",          :limit => 25
+    t.integer  "section_id",                       :default => 1
+    t.string   "section_label",      :limit => 25
+    t.integer  "activity_code",                    :default => 0
+    t.string   "activity_label",     :limit => 25
+    t.string   "app_activity_label", :limit => 25
+    t.integer  "item_id",                          :default => 0
+    t.integer  "item_revision_id",                 :default => 0
+    t.string   "item_fingerprint",   :limit => 64
+    t.integer  "source_id",                        :default => 0
     t.string   "source_model"
-    t.string   "ip_address",       :limit => 45
-    t.string   "fingerprint",      :limit => 64
+    t.string   "source_table"
+    t.string   "ip_address",         :limit => 45
+    t.string   "fingerprint",        :limit => 64
     t.text     "additionaldata"
     t.datetime "activity_at"
     t.datetime "created_at"
@@ -73,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20170301165735) do
 
   add_index "app_activities", ["activity_at", "person_id", "app_id", "section_id", "activity_code", "ip_address"], :name => "fields_ndx"
   add_index "app_activities", ["fingerprint"], :name => "fingerprint_ndx", :unique => true
+  add_index "app_activities", ["item_fingerprint"], :name => "item_fingerprint_ndx"
 
   create_table "auth_approvals", :force => true do |t|
     t.integer  "person_id",  :default => 0, :null => false
