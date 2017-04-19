93757ebb35611c495725dabbd8e32bac41b3e693
Jason Adam Young <jayoung@extension.org>
2017-04-19 10:47:04 -0400

    add some additional count fields

--- db/migrate/20170417160831_add_app_activity.rb
+++ db/migrate/20170417160831_add_app_activity.rb
@@ -2,27 +2,25 @@ class AddAppActivity < ActiveRecord::Migration
   def change
 
     create_table "app_activities", :force => true do |t|
-      t.integer  "trackable_id",       :default => 0
-      t.string   "trackable_type",     :limit => 25
-      t.integer  "app_id",             :default => 0
-      t.string   "app_label",          :limit => 25
-      t.integer  "app_source_type",    :default => 0
-      t.integer  "section_id",         :default => 1
-      t.string   "section_label",      :limit => 25
-      t.integer  "activity_code",      :default => 0
-      t.string   "activity_label",     :limit => 25
-      t.string   "app_activity_label", :limit => 25
+      t.integer  "trackable_id",          :default => 0
+      t.string   "trackable_type",        :limit => 25
+      t.integer  "app_id",                :default => 0
+      t.integer  "activity_code",         :default => 0
+      t.integer  "section_id",            :default => 1
+      t.string   "section_label",         :limit => 25
       t.string   "source_model"
-      t.integer  "year"
-      t.integer  "month"
-      t.integer  "quarter"
-      t.integer  "item_count"
-      t.integer  "activity_count"
-      t.text     "additionaldata"
-      t.datetime "created_at"
+      t.integer  "year",               :default => 0
+      t.integer  "month",              :default => 0
+      t.integer  "quarter",            :default => 0
+      t.integer  "item_count",         :default => 0
+      t.integer  "activity_count",     :default => 0
+      t.integer  "person_count",       :default => 0
+      t.integer  "pool_count",         :default => 0
+      t.text     "additionaldata",     :limit => 16777215
+      t.timestamps
     end
 
-    add_index "app_activities", ["trackable_id","trackable_type","activity_code","year","month","quarter"], :name => "trackable_ndx"
+    add_index "app_activities", ["trackable_id","trackable_type","activity_code","year","month","quarter"], :name => "trackable_ndx", :unique => true
 
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -65,27 +65,26 @@ ActiveRecord::Schema.define(:version => 20170417160831) do
   add_index "admin_roles", ["person_id", "applabel"], :name => "admin_ndx", :unique => true
 
   create_table "app_activities", :force => true do |t|
-    t.integer  "trackable_id",                     :default => 0
-    t.string   "trackable_type",     :limit => 25
-    t.integer  "app_id",                           :default => 0
-    t.string   "app_label",          :limit => 25
-    t.integer  "app_source_type",                  :default => 0
-    t.integer  "section_id",                       :default => 1
-    t.string   "section_label",      :limit => 25
-    t.integer  "activity_code",                    :default => 0
-    t.string   "activity_label",     :limit => 25
-    t.string   "app_activity_label", :limit => 25
+    t.integer  "trackable_id",                       :default => 0
+    t.string   "trackable_type", :limit => 25
+    t.integer  "app_id",                             :default => 0
+    t.integer  "activity_code",                      :default => 0
+    t.integer  "section_id",                         :default => 1
+    t.string   "section_label",  :limit => 25
     t.string   "source_model"
-    t.integer  "year"
-    t.integer  "month"
-    t.integer  "quarter"
-    t.integer  "item_count"
-    t.integer  "activity_count"
-    t.text     "additionaldata"
-    t.datetime "created_at"
+    t.integer  "year",                               :default => 0
+    t.integer  "month",                              :default => 0
+    t.integer  "quarter",                            :default => 0
+    t.integer  "item_count",                         :default => 0
+    t.integer  "activity_count",                     :default => 0
+    t.integer  "person_count",                       :default => 0
+    t.integer  "pool_count",                         :default => 0
+    t.text     "additionaldata", :limit => 16777215
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
   end
 
-  add_index "app_activities", ["trackable_id", "trackable_type", "activity_code", "year", "month", "quarter"], :name => "trackable_ndx"
+  add_index "app_activities", ["trackable_id", "trackable_type", "activity_code", "year", "month", "quarter"], :name => "trackable_ndx", :unique => true
 
   create_table "auth_approvals", :force => true do |t|
     t.integer  "person_id",  :default => 0, :null => false
