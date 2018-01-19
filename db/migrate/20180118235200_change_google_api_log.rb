3dd5966c4b769aea119b3e95044f4b8047c10037
Jason Young <jay@outfielding.net>
2018-01-19 14:38:36 -0500

    whitespace

--- db/migrate/20180118235200_change_google_api_log.rb
+++ db/migrate/20180118235200_change_google_api_log.rb
@@ -6,7 +6,6 @@ class ChangeGoogleApiLog < ActiveRecord::Migration
     remove_column(:google_api_logs, :errordata)
     rename_column(:google_api_logs, 'account_id', 'user_key')
     rename_column(:google_api_logs, 'group_id', 'group_key')
-
     add_column(:google_api_logs, :has_error, :boolean, null: false, default: false)
     add_column(:google_api_logs, :error_class, :string, null: true)
     add_column(:google_api_logs, :error_message, :string, null: true)
