fa78deefc628e635669d51a4014fe35c1e66f420
Ben MacNeill <ben.macneill@gmail.com>
2021-05-13 09:54:51 -0400

    strip out community actions, replace with explanation page

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -19,6 +19,7 @@ class CommunitiesController < ApplicationController
   end
 
   def show
+    return redirect_to(communities_path)
     # will raise ActiveRecord::RecordNotFound on not found
     @community = Community.find_by_shortname_or_id(params[:id])
     member_breadcrumbs
@@ -27,6 +28,7 @@ class CommunitiesController < ApplicationController
   end
 
   def edit
+    return redirect_to(communities_path)
     @community = Community.find(params[:id])
     member_breadcrumbs(['Edit community settings'])
 
@@ -123,6 +125,7 @@ class CommunitiesController < ApplicationController
   end
 
   def connections
+    return redirect_to(communities_path)
     # will raise ActiveRecord::RecordNotFound on not found
     @community = Community.find_by_shortname_or_id(params[:id])
     allowed_connections = Community::CONNECTION_CONDITIONS.keys
@@ -142,6 +145,7 @@ class CommunitiesController < ApplicationController
   end
 
   def gallery
+    return redirect_to(communities_path)
     @no_show_navtabs = true
     @community = Community.find_by_shortname_or_id(params[:id])
     if(current_person)
@@ -169,6 +173,7 @@ class CommunitiesController < ApplicationController
   end
 
   def invite
+    return redirect_to(communities_path)
     @community = Community.find_by_shortname_or_id(params[:id])
     member_breadcrumbs(['Invite colleagues'])
 
@@ -226,6 +231,7 @@ class CommunitiesController < ApplicationController
   end
 
   def activity
+    return redirect_to(communities_path)
     if(params[:id])
       @community = Community.find_by_shortname_or_id(params[:id])
       member_breadcrumbs(['Activity'])
